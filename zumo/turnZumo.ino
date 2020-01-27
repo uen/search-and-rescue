5f83f3fc7cc7ad05dee3c067d5593b08fc481430
Manolis Vrondakis <manolisvrondakis@gmail.com>
2020-01-27 11:39:32 +0000

    Adjust front sensor threshhold

--- zumo/turnZumo.ino
+++ zumo/turnZumo.ino
@@ -1,32 +1,28 @@
 #include <Arduino.h>
 #include <Zumo32U4.h>
 
-// t junction
-#define DEGREE_MODIFIER 1 // Remove 10 degrees from the total degrees to turn. As the gyro is inaccurate
-#define TURN_SPEED 80
+
+#define DEGREE_MODIFIER 1 // * by the total degrees to turn. As the gyro is inaccurate it sometimes needs adjusting
+#define TURN_SPEED 70
 
 void turnZumo(int degrees){
     turnZumo(degrees, false);
 }
 
 void turnZumo(int degrees, bool doNotStore){
+    // Turn the right way
     int directionModifier = -1;
     if(degrees < 0){
         directionModifier = 1;
     }
 
-    degrees = degrees * DEGREE_MODIFIER;
-
-    // if(!doNotStore)
-        // Serial1.println("Turning " + String(degrees));
-
-    
+    degrees = degrees * DEGREE_MODIFIER;    
     motors.setSpeeds(TURN_SPEED * directionModifier, -TURN_SPEED * directionModifier);
-  
+
+    // Reset turn sensors
     turnSensorReset();
 
-
-    // TODO FIX BAD CODE 
+    // Turn until we have gone far enough
     if(degrees <= 0){
         int angle = 0;
         do {
@@ -36,23 +32,19 @@ void turnZumo(int degrees, bool doNotStore){
         } while(angle > degrees);
     }
 
-
     if(degrees > 0){
         int angle = 0;
         do {
             delay(1);
             turnSensorUpdate();
             angle = (((int32_t)turnAngle >> 16) * 360) >> 16;
-            // delay(100);
-            // Serial1.println("zumo:We have currently turned" + String(angle));
         } while(angle <= (degrees));
     }
 
+    // Record the turn for the path home
     if(!doNotStore) recordTurn(degrees);
     
     delay(100);
-    
-
     motors.setSpeeds(0, 0);
 }
 
@@ -65,5 +57,4 @@ void turnZumo(int len, String data){
         turnZumo(90);
 
     // autonomousMode();
-    // TODO: 
 }
